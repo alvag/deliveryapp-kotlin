f8c54883df41da3a017f2186d4b08ce9b71f0338
Max Alva <alva85@gmail.com>
2022-01-14 12:54:49 -0500

    Obteniendo valores de los campos de texto

--- app/src/main/java/com/maxalva/deliveryapp/activities/MainActivity.kt
+++ app/src/main/java/com/maxalva/deliveryapp/activities/MainActivity.kt
@@ -3,24 +3,49 @@ package com.maxalva.deliveryapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import com.maxalva.deliveryapp.R
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     private lateinit var imageViewGoToRegister: ImageView
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var btnLogin: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         imageViewGoToRegister = findViewById(R.id.imageview_go_to_register)
-
         imageViewGoToRegister.setOnClickListener { goToRegister() }
+
+        editTextEmail = findViewById(R.id.edit_text_email)
+        editTextPassword = findViewById(R.id.edit_text_password)
+
+        btnLogin = findViewById(R.id.btn_login)
+        btnLogin.setOnClickListener { login() }
+    }
+
+    private fun login() {
+        val email = editTextEmail.text.toString()
+        val password = editTextPassword.text.toString()
+
+        Log.d(TAG, "El email es $email")
+        Log.d(TAG, "El email es $password")
     }
 
     private fun goToRegister() {
         val i = Intent(this, RegisterActivity::class.java)
         startActivity(i)
     }
+
+
 }
--- app/src/main/java/com/maxalva/deliveryapp/activities/RegisterActivity.kt
+++ app/src/main/java/com/maxalva/deliveryapp/activities/RegisterActivity.kt
@@ -3,20 +3,56 @@ package com.maxalva.deliveryapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import com.maxalva.deliveryapp.R
 
 class RegisterActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "RegisterActivity"
+    }
+
     private lateinit var imageViewGoToLogin: ImageView
+    private lateinit var editTextName: EditText
+    private lateinit var editTextLastName: EditText
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextPhone: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var editTextConfirmPassword: EditText
+    private lateinit var btnRegister: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         imageViewGoToLogin = findViewById(R.id.imageview_go_to_login)
-
         imageViewGoToLogin.setOnClickListener { goToLogin() }
+
+        editTextName = findViewById(R.id.edit_text_name)
+        editTextLastName = findViewById(R.id.edit_text_lastname)
+        editTextEmail = findViewById(R.id.edit_text_email)
+        editTextPhone = findViewById(R.id.edit_text_phone)
+        editTextPassword = findViewById(R.id.edit_text_password)
+        editTextConfirmPassword = findViewById(R.id.edit_text_confirm_password)
+
+        btnRegister = findViewById(R.id.btn_register)
+        btnRegister.setOnClickListener { register() }
+    }
+
+    private fun register() {
+        val name = editTextName.text.toString()
+        val lastName = editTextLastName.text.toString()
+        val email = editTextEmail.text.toString()
+        val phone = editTextPhone.text.toString()
+        val password = editTextPassword.text.toString()
+        val confirmPassword = editTextConfirmPassword.text.toString()
+
+        Log.d(TAG, "El nombre es $name")
+        Log.d(TAG, "El apellido es $lastName")
+        Log.d(TAG, "El email es $email")
     }
 
     private fun goToLogin() {
