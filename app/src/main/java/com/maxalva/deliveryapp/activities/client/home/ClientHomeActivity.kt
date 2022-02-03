d97283a429046e82a484ac133574eb2f3f686fbc
Max Alva Garcia <maxrober.alvagarcia@everis.nttdata.com>
2022-02-02 22:15:42 -0500

    menu

--- app/src/main/java/com/maxalva/deliveryapp/activities/client/home/ClientHomeActivity.kt
+++ app/src/main/java/com/maxalva/deliveryapp/activities/client/home/ClientHomeActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.gson.Gson
 import com.maxalva.deliveryapp.R
 import com.maxalva.deliveryapp.Utils.SharedPref
 import com.maxalva.deliveryapp.activities.MainActivity
+import com.maxalva.deliveryapp.fragments.client.ClientCategoriesFragment
+import com.maxalva.deliveryapp.fragments.client.ClientOrdersFragment
+import com.maxalva.deliveryapp.fragments.client.ClientProfileFragment
 import com.maxalva.deliveryapp.models.User
 
 class ClientHomeActivity : AppCompatActivity() {
@@ -19,19 +23,48 @@ class ClientHomeActivity : AppCompatActivity() {
 
     private lateinit var sharedPref: SharedPref
 
-    private lateinit var btnLogout: Button;
+    //    private lateinit var btnLogout: Button;
+    private lateinit var bottomNavigation: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_home)
 
+        openFragment(ClientCategoriesFragment())
+
         sharedPref = SharedPref(this)
-
-        btnLogout = findViewById(R.id.btn_log_out)
-
-        btnLogout.setOnClickListener { logout() }
-
         getUserFromSession()
+
+//        btnLogout = findViewById(R.id.btn_log_out)
+//        btnLogout.setOnClickListener { logout() }
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_home -> {
+                    openFragment(ClientCategoriesFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.item_orders -> {
+                    openFragment(ClientOrdersFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.item_profile -> {
+                    openFragment(ClientProfileFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+
+            return@setOnNavigationItemSelectedListener false
+        }
+
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
     private fun logout() {
